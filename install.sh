aed3686cbb3973d826a97f4c87ea8a5ab108ef32
jashanpreet-singh-99 <singh872@uwindsor.ca>
2024-10-06 23:16:27 -0400

    Updated with monitor binary

--- install.sh
+++ install.sh
@@ -12,22 +12,10 @@ then
 fi
 cd ~/ccminer
 
-GITHUB_RELEASE_JSON=$(curl --silent "https://api.github.com/repos/Oink70/CCminer-ARM-optimized/releases?per_page=1" | jq -c '[.[] | del (.body)]')
-GITHUB_DOWNLOAD_URL=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].browser_download_url")
-GITHUB_DOWNLOAD_NAME=$(echo $GITHUB_RELEASE_JSON | jq -r ".[0].assets[0].name")
-
-echo "Downloading latest release: $GITHUB_DOWNLOAD_NAME"
-
-wget ${GITHUB_DOWNLOAD_URL} -P ~/ccminer
-
 wget https://raw.githubusercontent.com/jashanpreet-singh-99/Android-Mining/main/config.json -P ~/ccminer
+wget https://raw.githubusercontent.com/jashanpreet-singh-99/Android-Mining/main/ccminer -P ~/ccminer
 # https://raw.githubusercontent.com/jashanpreet-singh-99/Android-Mining/refs/heads/main/config.json
 
-if [ -f ~/ccminer/ccminer ]
-then
-  mv ~/ccminer/ccminer ~/ccminer/ccminer_old
-fi
-mv ~/ccminer/${GITHUB_DOWNLOAD_NAME} ~/ccminer/ccminer
 chmod +x ~/ccminer/ccminer
 
 cat << EOF > ~/ccminer/start.sh
@@ -47,6 +35,9 @@ printf 'start: ~/.ccminer/start.sh\n'
 printf 'stop: screen -X -S CCminer quit\n'
 printf '\nmonitor mining: screen -x CCminer\n'
 printf "exit monitor: 'CTRL-a' followed by 'd'\n\n"
+
+alias check_m='screen -x CCminer'
+alias stop_m='screen -X -S CCminer quit'
 EOF
 chmod +x start.sh
 
