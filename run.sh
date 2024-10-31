28f9619af0b336a9c23468f6aee0e92eb68872f7
Jashanpreet singh <83163927+jashanpreet-singh-99@users.noreply.github.com>
2024-10-31 01:58:53 -0400

    Update address run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./ccminer -r 20 -b 23500 -a verus -o stratum+tcp://na.luckpool.net:3956 -u RXvnNTuPw2Wzouq7CpJ6BhSXcAXpp2aW6a.PixelNew -p x -t 8
+./ccminer -r 20 -b 23500 -a verus -o stratum+tcp://na.luckpool.net:3956 -u RAfXjJS4skCjsLFQ9JpgmCxxehtnCxAuhx.PixelNew -p x -t 8
